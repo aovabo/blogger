2ce6e8eb618d233ef4f8c65668daf5e1f8439100
aovabo <45472105+aovabo@users.noreply.github.com>
2023-05-21 04:41:29 -0400

    Update my_blog.ts

--- testdata/my_blog.ts
+++ testdata/my_blog.ts
@@ -3,7 +3,7 @@ import blog from "../blog.tsx";
 blog({
   author: "Xela-R",
   title: "My Innovation Orchard",
-  description: "Sharing my thoughts and ideas about the projects that I'm working on.",
+  description: "Thoughts and ideas about the projects that I'm working on.",
   avatar: "https://deno-avatar.deno.dev/avatar/blog.svg",
   avatarClass: "rounded-full",
   links: [
