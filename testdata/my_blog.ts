b6f075e6b0557846191f026c26b64ec9655d894e
aovabo <45472105+aovabo@users.noreply.github.com>
2023-05-23 03:33:57 -0400

    Update my_blog.ts

--- testdata/my_blog.ts
+++ testdata/my_blog.ts
@@ -7,9 +7,9 @@ blog({
   avatar: "https://deno-avatar.deno.dev/avatar/blog.svg",
   avatarClass: "rounded-full",
   links: [
-    { title: "bot@deno.com", url: "mailto:bot@deno.com" },
-    { title: "GitHub", url: "https://github.com/denobot" },
-    { title: "Twitter", url: "https://twitter.com/denobot" },
+    { title: "bot@ovabo.com", url: "mailto:bot@ovabo.com" },
+    { title: "GitHub", url: "https://github.com/ovabo" },
+    { title: "Twitter", url: "https://twitter.com/aovabo" },
   ],
 });
 
