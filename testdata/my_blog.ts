4abc03721f98cd50a119a4442a3d6b8e8f1b40b2
aovabo <45472105+aovabo@users.noreply.github.com>
2023-06-15 21:50:23 -0400

    Update my_blog.ts

--- testdata/my_blog.ts
+++ testdata/my_blog.ts
@@ -2,12 +2,12 @@ import blog from "../blog.tsx";
 
 blog({
   author: "Xela-Robavo",
-  title: "Alexander's Innovation Orchard",
+  title: "Ovabor's Innovation Orchard",
   description: "Chronicles of my experimental and passion project.",
   avatar: "https://deno-avatar.deno.dev/avatar/blog.svg",
   avatarClass: "rounded-full",
   links: [
-    { title: "bot@ovabo.com", url: "mailto:research@ovabor.xyz" },
+    { title: "blog@ovabor.xyz", url: "mailto:research@ovabor.xyz" },
     { title: "GitHub", url: "https://github.com/ovabo" },
     { title: "Twitter", url: "https://twitter.com/aovabo" },
   ],
