2aaf4823405da87725758c1faa8a57645b29d884
aovabo <45472105+aovabo@users.noreply.github.com>
2023-09-10 20:06:00 -0400

    Update components.tsx

--- components.tsx
+++ components.tsx
@@ -236,12 +236,12 @@ function Footer(props: { author?: string }) {
     <footer class="mt-20 pb-16 lt-sm:pb-8 lt-sm:mt-16">
       <p class="flex items-center gap-2.5 text-gray-400/800 dark:text-gray-500/800 text-sm">
         <span>
-          Powered by{" "}
+          Blog by{" "}
           <a
             class="inline-flex items-center gap-1 underline hover:text-gray-800 dark:hover:text-gray-200 transition-colors"
-            href="https://deno.land/x/blog"
+            href="https://github.com/ovabo"
           >
-            Deno Blog
+            Alexander
           </a>
         </span>
         <a
