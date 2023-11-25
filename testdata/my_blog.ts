f4af8225159f83d8ee17b650d6dc75e6241edb3b
aovabo <45472105+aovabo@users.noreply.github.com>
2023-11-24 22:23:11 -0500

    Update my_blog.ts

--- testdata/my_blog.ts
+++ testdata/my_blog.ts
@@ -1,14 +1,29 @@
-import blog from "../blog.tsx";
+import blog, { ga, redirects } from "https://deno.land/x/blog/blog.tsx";
+// import { unocss_opts } from "./unocss.ts";
 
 blog({
   author: "Xela Robavo",
-  title: "Ovabo's Innovation Orchard",
-  description: "Chronicles of my experimental and passion project.",
+  title: "Xela Robavo's Innovation Orchard",
+  description: "Chronicles of My Experimental and Passion Projects.",
   avatar: "alex-ovabor-black.png",
-  avatarClass: "square-full",
+  avatarClass: "rounded-full",
   links: [
-    { title: "blog@ovabor.xyz", url: "mailto:blog@ovabor.xyz" },
-    { title: "GitHub", url: "https://github.com/ovabo" },
+    { title: "Email", url: "mailto:aovabo@alexovabor.com" },
+    { title: "GitHub", url: "https://github.com/aovabo" },
     { title: "Twitter", url: "https://twitter.com/aovabo" },
   ],
+  lang: "en",
+  // localised format based on https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat
+  dateFormat: (date) =>
+    new Intl.DateTimeFormat("en-US", { dateStyle: "long" }).format(date),
+//  middlewares: [
+//    ga("UA-XXXXXXXX-X"),
+//    redirects({
+//      "/foo": "/my_post",
+      // you can skip leading slashes too
+//      "bar": "my_post2",
+    }),
+  ],
+  // unocss: unocss_opts, // check https://github.com/unocss/unocss
+  favicon: "alexfavicon.png",
 });
