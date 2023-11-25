037faffdf1906a83228dd5b811fa4736a58f431f
aovabo <45472105+aovabo@users.noreply.github.com>
2023-11-24 21:35:11 -0500

    Update my_blog.ts

--- testdata/my_blog.ts
+++ testdata/my_blog.ts
@@ -1,16 +1,15 @@
 import blog from "../blog.tsx";
+import { unocss_opts } from "./unocss.ts";
 
 blog({
   author: "Xela Robavo",
   title: "Ovabo's Innovation Orchard",
   description: "Chronicles of my experimental and passion project.",
   avatar: "alex-ovabor-black.png",
-  avatarClass: "rounded-full",
+  avatarClass: "square-full",
   links: [
     { title: "blog@ovabor.xyz", url: "mailto:blog@ovabor.xyz" },
     { title: "GitHub", url: "https://github.com/ovabo" },
     { title: "Twitter", url: "https://twitter.com/aovabo" },
   ],
 });
-
-
